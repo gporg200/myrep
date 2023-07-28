5227f7159c50b60fd68adc812cf570f8b38ffaf7
m <m@m.com>
2023-07-28 13:18:05 -0700

    first

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.set('views','./views');
 
+var homeuser = 0
+var aboutuser = 0
+
+
 app.get('/', function(req, res){
-   res.render('view1');
-});
+
+		res.render('view1');
+		homeuser +=1
+		console.log(`homepage users: ${homeuser}`)
+		});
 
 app.get('/about', function(req, res){
-   res.render('about');
-});
+		res.render('about');
+		aboutuser += 1
+		console.log(`aboutpage users: ${aboutuser}`)
+		});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+		console.log(`http://localhost:${port}`)
+		})
