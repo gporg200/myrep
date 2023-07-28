467ee81e8619414806eaba036297f7937ef9463f
gporg200 <greenporg200@gmail.com>
2023-07-27 18:26:12 -0700

    'links'

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-var i = 0;
-
+app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.set('views','./views');
 
 app.get('/', function(req, res){
    res.render('view1');
+});
+
+app.get('/about', function(req, res){
+   res.render('about');
 });
 
 app.listen(port, () => {
